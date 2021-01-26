8705ec5a6d5d2c9d95a9b156577591e0c26596c7
golubenkoff <33029016+golubenkoff@users.noreply.github.com>
2021-01-26 15:28:02 +0200

    Add files via upload

---
+++ Pradeep_Ext11-Report-All.ps1
@@ -0,0 +1,7 @@
+﻿param(
+$FileName = "C:\Scripts\Pradeep_Source.csv"
+)
+
+$Report = import-csv $FileName -Encoding UTF8 | %{ $Name = $null ;  $Name = $_.Name
+Get-ADUser -Filter {Name -eq $Name} -Properties extensionattribute11,mail,DisplayName | Select Name,DisplayName,Mail,extensionattribute11
+}  | export-csv -NoTypeInformation -Encoding UTF8 -Path $($FileName -replace "\.csv","_Report_$(get-date -f 'ddMMyyyyHHmm').csv")
