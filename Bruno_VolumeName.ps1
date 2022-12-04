6348758d61c9d69de9f773f45b52de1627e41e9a
Andriy Golubenkov FUIB VM <ag@pumb.ua>
2022-12-04 13:05:50 +0200

    Volume Names

---
+++ Bruno_VolumeName.ps1
@@ -0,0 +1,27 @@
+﻿function Read-FolderBrowserDialog([string]$Message, [string]$InitialDirectory, [switch]$NoNewFolderButton) {
+    $browseForFolderOptions = 0
+    if ($NoNewFolderButton) { $browseForFolderOptions += 512 }
+
+    $app = New-Object -ComObject Shell.Application
+    $folder = $app.BrowseForFolder(0, $Message, $browseForFolderOptions, $InitialDirectory)
+    if ($folder) { $selectedDirectory = $folder.Self.Path } else { $selectedDirectory = '' }
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($app) > $null
+    return $selectedDirectory
+}
+
+
+
+$FilePath = Read-FolderBrowserDialog -Message 'Select Folder to Get Volume Name' -InitialDirectory 'MyComputer'
+Write-Host Selected folder: $FilePath
+
+$DirInfo = [System.IO.DirectoryInfo]$FilePath
+
+# Method 1
+Write-Host 'Method 1: ' -ForegroundColor Cyan -NoNewline ; Write-Host ([System.IO.DriveInfo]::GetDrives() | Where-Object Name -EQ $DirInfo.Root.Name).VolumeLabel
+
+# Method 2
+Write-Host 'Method 2: ' -ForegroundColor Cyan -NoNewline ; Write-Host (Get-Partition -DriveLetter $DirInfo.Root.Name[0] | Get-Volume).FileSystemLabel
+
+# Method 3
+Write-Host 'Method 3: ' -ForegroundColor Cyan -NoNewline ; Write-Host (Get-WmiObject win32_volume | Where-Object { $_.DriveLetter } | Where-Object Name -EQ $DirInfo.Root.Name).Label
+
---
+++ Get-DiskDevicePaths.ps1
@@ -0,0 +1,62 @@
+Function Get-DeviceFromPath {
+    param(
+        $driveLetter
+    )
+
+    $DynAssembly = New-Object System.Reflection.AssemblyName('SysUtils')
+    $AssemblyBuilder = [AppDomain]::CurrentDomain.DefineDynamicAssembly($DynAssembly, [Reflection.Emit.AssemblyBuilderAccess]::Run)
+    $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule('SysUtils', $False)
+
+    # Define [Kernel32]::QueryDosDevice method
+    $TypeBuilder = $ModuleBuilder.DefineType('Kernel32', 'Public, Class')
+    $PInvokeMethod = $TypeBuilder.DefinePInvokeMethod('QueryDosDevice', 'kernel32.dll', ([Reflection.MethodAttributes]::Public -bor [Reflection.MethodAttributes]::Static), [Reflection.CallingConventions]::Standard, [UInt32], [Type[]]@([String], [Text.StringBuilder], [UInt32]), [Runtime.InteropServices.CallingConvention]::Winapi, [Runtime.InteropServices.CharSet]::Auto)
+    $DllImportConstructor = [Runtime.InteropServices.DllImportAttribute].GetConstructor(@([String]))
+    $SetLastError = [Runtime.InteropServices.DllImportAttribute].GetField('SetLastError')
+    $SetLastErrorCustomAttribute = New-Object Reflection.Emit.CustomAttributeBuilder($DllImportConstructor, @('kernel32.dll'), [Reflection.FieldInfo[]]@($SetLastError), @($true))
+    $PInvokeMethod.SetCustomAttribute($SetLastErrorCustomAttribute)
+    $Kernel32 = $TypeBuilder.CreateType()
+
+    $Max = 65536
+    $StringBuilder = New-Object System.Text.StringBuilder($Max)
+    $ReturnLength = $Kernel32::QueryDosDevice($driveLetter, $StringBuilder, $Max)
+
+    if ($ReturnLength) {
+        return $StringBuilder.ToString()
+    } else {
+        return $null
+    }
+}
+
+Function Get-DrivesPaths {
+    param()
+    # Biuild System Assembly in order to call Kernel32:QueryDosDevice.
+    $DynAssembly = New-Object System.Reflection.AssemblyName('SysUtils')
+    $AssemblyBuilder = [AppDomain]::CurrentDomain.DefineDynamicAssembly($DynAssembly, [Reflection.Emit.AssemblyBuilderAccess]::Run)
+    $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule('SysUtils', $False)
+
+    # Define [Kernel32]::QueryDosDevice method
+    $TypeBuilder = $ModuleBuilder.DefineType('Kernel32', 'Public, Class')
+    $PInvokeMethod = $TypeBuilder.DefinePInvokeMethod('QueryDosDevice', 'kernel32.dll', ([Reflection.MethodAttributes]::Public -bor [Reflection.MethodAttributes]::Static), [Reflection.CallingConventions]::Standard, [UInt32], [Type[]]@([String], [Text.StringBuilder], [UInt32]), [Runtime.InteropServices.CallingConvention]::Winapi, [Runtime.InteropServices.CharSet]::Auto)
+    $DllImportConstructor = [Runtime.InteropServices.DllImportAttribute].GetConstructor(@([String]))
+    $SetLastError = [Runtime.InteropServices.DllImportAttribute].GetField('SetLastError')
+    $SetLastErrorCustomAttribute = New-Object Reflection.Emit.CustomAttributeBuilder($DllImportConstructor, @('kernel32.dll'), [Reflection.FieldInfo[]]@($SetLastError), @($true))
+    $PInvokeMethod.SetCustomAttribute($SetLastErrorCustomAttribute)
+    $Kernel32 = $TypeBuilder.CreateType()
+
+    $Max = 65536
+    $StringBuilder = New-Object System.Text.StringBuilder($Max)
+
+    Get-WmiObject Win32_Volume | Where-Object { $_.DriveLetter } | ForEach-Object {
+        $ReturnLength = $Kernel32::QueryDosDevice($_.DriveLetter, $StringBuilder, $Max)
+
+        if ($ReturnLength) {
+            $DriveMapping = @{
+                DriveLetter = $_.DriveLetter
+                DevicePath  = $StringBuilder.ToString()
+            }
+
+            return New-Object PSObject -Property $DriveMapping
+        }
+    }
+}
+
