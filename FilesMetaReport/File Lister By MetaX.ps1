3101f7417963391743d043a070a8ac6864ea9819
Andriy Golubenkov FUIB VM <ag@pumb.ua>
2023-03-13 13:29:22 +0200

    FIles MetadataReport

---
+++ FilesMetaReport/File Lister By MetaX.ps1
@@ -0,0 +1,99 @@
+﻿#Get-ChildItem -Recurse -File | % fullname | Export-Excel D:\TEMP\MEDIA_ALL.xlsx -Append
+
+# D:\Music (Full) 1 Hour 30 Minutes 13096
+# D:\Music (MetaRoot) 1 Hour Count 5259
+# D:\Music (MetaRecurse) 25 Minutes 3771
+# R:\Music (Full) 6 Hours 40 Minutes Count 1802
+# R:\Music (MetaRoot) 5 Hours 00 Minutes Count
+# R:\Music (MetaRecurse) 1 Hour 35 Minutes Count
+# MEDIA BACKUP1 23 Hours 20 Minutes
+# MEDIA BACKUP2 25 Hours
+# MEDIA5TB 15 Hours 30 Minutes
+
+# Start Transcript
+Start-Transcript D:\LOGS\MEDIA_TRANSCRIPT.txt
+
+# To execute the script without agreeing with the execution policy
+Set-ExecutionPolicy Bypass -Scope Process
+
+# Import the PS1 file to access the functions
+Import-module .\Get-FileMetaDataReturnObject.ps1 -Force
+
+# Defines the directory where the files will be, to get their metadata
+$FilePath = New-Object System.Windows.Forms.FolderBrowserDialog
+$FilePath.RootFolder = "MyComputer"
+$FilePath.ShowDialog()
+Write-Host Selected folder: $FilePath.SelectedPath -ForegroundColor Yellow
+
+# Lets get the Volume Name
+$FPSubString = $FilePath.SelectedPath.Substring(0,2)
+$VolumeName = Get-CimInstance CIM_LogicalDisk | Where-Object {$_.Name -eq $FPSubString} | Select-Object -ExpandProperty VolumeName -Last 1
+
+# Lets create File Name
+$Dir = $FilePath.SelectedPath.Replace(':\','-')
+$Dir = $Dir.Replace('\','-')
+$Dir = "D:\Temp\LIST-${VolumeName}-${Dir}.xlsx"
+Write-Host Filename = $Dir -ForegroundColor Yellow
+Start-Sleep -Seconds 30
+
+#Loads metadata into the variable
+Get-Date -Format "dddd dd MMMM yyyy hh:mm tt"
+Write-Host Processing MetaRoot
+$MetaRoot = Get-FileMetaData -Folder $FilePath.SelectedPath
+Get-Date -Format "dddd dd MMMM yyyy hh:mm tt"
+Write-Host Processing MetaRecurse
+$MetaRecurse = Get-FileMetaData -Folder (Get-ChildItem $FilePath.SelectedPath -Recurse -Directory).FullName
+Get-Date -Format "dddd dd MMMM yyyy hh:mm tt"
+
+######## LETS EXPORT THE DATA TO AN EXCEL FILE ########
+# Wild card selection
+#$ExcelDataRoot = $MetaRoot | Where-Object {$_.Name -match 'Jessie'}
+#$ExcelDataRecurse = $MetaRecurse | Where-Object {$_.Name -match 'Jessie'}
+
+# Exclude Folder Names and iTunes type files
+$ExcelDataRoot = $MetaRoot | Where-Object {$_.'Item type' -ne "File folder"}
+$ExcelDataRecurse = $MetaRecurse | Where-Object {$_.'Item type' -ne "File folder" -and $_.'Item type' -ne "ITC2 File" -and
+    $_.'Item type' -ne "iTunes Database File"}
+
+$ExcelDataRoot    | Select-Object 'File Location', Name, Path, Title, 'Item Type', Genre, Length, Album, 'Album Artist', Size | Export-Excel -Path $Dir | Sort-Object 'File Location', Name
+$ExcelDataRecurse | Select-Object 'File Location', Name, Path, Title, 'Item Type', Genre, Length, Album, 'Album Artist', Size | Export-Excel -Path $Dir -Append | Sort-Object 'File Location', Name
+
+# Include everything
+#$MetaRoot | Select-Object 'File Location', Name, Path, 'Item Type', Genre, Length, Album, 'Album Artist', Size | Export-Excel -Path $Dir | Sort-Object 'File Location', Name
+#$MetaRecurse | Select-Object 'File Location', Name, Path, 'Item Type', Genre, Length, Album, 'Album Artist' | Export-Excel -Path $Dir | Sort-Object 'File Location', Name
+
+######## LETS OPEN THE EXPORTED FILE ########
+$Excel = New-Object -ComObject Excel.Application
+$Excel.Windowstate = "xlMaximized"
+$Book = $Excel.Workbooks.Open($Dir)
+$Sheet = $Book.Worksheets.Item(1)
+$Excel.Visible = $true
+
+######## LETS ADD THE FILE COUNT ########
+$RowCount = $Excel.ActiveSheet.UsedRange.Rows.Count - 1
+$Excel.ActiveCell.Cells.Item(1,10) = 'File Count'
+$Excel.ActiveCell.Cells.Item(1,11) = $RowCount
+$Excel.ActiveCell.Cells.Item(1,11).Interior.ColorIndex = 6
+
+######## LETS ADD VOLUME NAME COLUMN ########
+$Range1 = $Sheet.Range("A:A")
+$Range1.EntireColumn.Insert($ExcelShiftLeft)
+$Excel.ActiveCell.Cells.Item(1,1) = 'Volume Name'
+$Excel.ActiveCell.Cells.Item(2,1) = $VolumeName
+
+$VolumeCount = $RowCount + 1
+$Sheet = $Book.Worksheets.Item(1).Range("A2")
+$Sheet.Copy()
+$Excel.Range("A3:A$VolumeCount").Select()
+$Excel.Worksheets.Item(1).paste()
+
+######## LETS FORMAT THE EXPORTED FILE ########
+$Excel.Rows.Item(1).font.bold=$true
+$Excel.ActiveSheet.Cells.EntireColumn.autofit()
+$Excel.Rows.Item(2).Select()
+$Excel.ActiveWindow.FreezePanes = $true
+$Excel.Range("A1").Select()
+$Excel.Worksheets.Item(1).name = $VolumeName
+$Book.Save()
+Beep 600 1000 /s 50 /r 3
+Stop-Transcript
---
+++ FilesMetaReport/WF_Generate-MetadataReport.ps1
@@ -0,0 +1,168 @@
+﻿param($RootFolder,$LogFolder)
+
+# To execute the script without agreeing with the execution policy
+Set-ExecutionPolicy Bypass -Scope Process
+
+if (!$LogFolder) {
+    $ScriptPath = $MyInvocation.MyCommand.Path
+    $LogFolder = Split-Path -Parent $ScriptPath
+}
+
+# Start Transcript
+Start-Transcript $(Join-Path $LogFolder 'MEDIA_TRANSCRIPT.txt')
+
+#region Functions
+workflow WF_MetadataReportFile {
+    param(
+        [Parameter(Mandatory = $true)]$FolderPath,
+        $ThrottleLimit = 50
+    )
+
+    sequence {
+
+        $Files = Get-ChildItem -Path $FolderPath -File
+
+        if ($Files.Count -lt $ThrottleLimit) { $ThrottleLimit = $Files.Count }
+
+        Write-Verbose "ThrottleLimit: $ThrottleLimit"
+        Write-Verbose "Files Count  : $(($Files | Measure-Object).Count)"
+
+        foreach -Parallel -ThrottleLimit $ThrottleLimit ($file in $Files) {
+
+            InlineScript {
+                $shell = New-Object -ComObject Shell.Application
+                $File = $using:file
+                $folder = $shell.NameSpace($File.DirectoryName)
+                $fileObject = $folder.ParseName($file.Name)
+
+                [PSCustomObject]@{
+                    Folder      = $File.DirectoryName
+                    Name        = $file.Name
+                    Path        = $file.FullName
+                    Title       = $folder.GetDetailsOf($fileObject, 21)
+                    ItemType    = $folder.GetDetailsOf($fileObject, 2)
+                    Genre       = $folder.GetDetailsOf($fileObject, 16)
+                    Length      = $folder.GetDetailsOf($fileObject, 27)
+                    Album       = $folder.GetDetailsOf($fileObject, 14)
+                    AlbumArtist = $folder.GetDetailsOf($fileObject, 13)
+                    Size        = $file.Length
+                }
+            }
+        }
+    }
+}
+function Remove-InvalidFilePathCharacters {
+    param(
+        [string]$FilePath
+    )
+
+    # Get the invalid characters for a file path
+    $invalidChars = [System.IO.Path]::GetInvalidPathChars() | ForEach-Object { [regex]::Escape($_) }
+
+    # Remove the invalid characters from the file path
+    $cleanFilePath = [regex]::Replace($FilePath, "[$invalidChars]", '')
+
+    return $cleanFilePath
+}
+
+function Read-FolderBrowserDialog([string]$Message, [string]$InitialDirectory, [switch]$NoNewFolderButton)
+{
+    $browseForFolderOptions = 0
+    if ($NoNewFolderButton) { $browseForFolderOptions += 512 }
+
+    $app = New-Object -ComObject Shell.Application
+    $folder = $app.BrowseForFolder(0, $Message, $browseForFolderOptions, $InitialDirectory)
+    if ($folder) { $selectedDirectory = $folder.Self.Path } else { $selectedDirectory = '' }
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($app) > $null
+    return $selectedDirectory
+}
+
+
+#endregion Functions
+
+
+#region  Defines the directory where the files will be, to get their metadata
+Write-Host "[$(Get-Date -f 'dddd dd MMMM yyyy hh:mm:ss tt')] Select RootFolder: " -BackgroundColor cyan -ForegroundColor white
+
+$FilePath = Read-FolderBrowserDialog -Message 'Select Root Folder' -InitialDirectory  $(if ($RootFolder) { $RootFolder }else { 'MyComputer' })
+
+if ([string]::IsNullOrEmpty($FilePath)) {
+    Write-Host 'No Root Folder Selected... Aborting' -BackgroundColor Red -ForegroundColor White
+    break
+}
+
+Write-Host Selected folder: $FilePath -ForegroundColor Yellow
+
+# Lets get the Volume Name
+$FPSubString = $FilePath.Substring(0,2)
+$VolumeName = Get-CimInstance CIM_LogicalDisk | Where-Object { $_.Name -eq $FPSubString } | Select-Object -ExpandProperty VolumeName -Last 1
+
+# Lets create File Name
+$Dir = (Remove-InvalidFilePathCharacters $FilePath) -replace ':','-' -replace '\\','-'
+$Dir = Join-Path $LogFolder "LIST-${VolumeName}-${Dir}-$(get-date -f 'ddMMyyyyHHmm').xlsx"
+Write-Host "Filename = $Dir" -ForegroundColor Yellow
+#endregion  Defines the directory where the files will be, to get their metadata
+
+
+#region Processing Data
+Write-Host "[$(Get-Date -f 'dddd dd MMMM yyyy hh:mm:ss tt')] Processing Metadata for RootFolder: " $FilePath
+
+$StartMetaRoot = Get-Date
+$MetaRoot = WF_MetadataReportFile -FolderPath $FilePath
+$MetaRoot.count
+if (($MetaRoot | Measure-Object).count -gt 0) {
+    $MetaRoot.psobject.Properties.Remove('PSComputerName')
+    $MetaRoot.psobject.Properties.Remove('PSShowComputerName')
+    $MetaRoot.psobject.Properties.Remove('PSSourceJobInstanceId')
+}
+Get-Job | Remove-Job
+$EndMetaRoot = Get-Date
+Write-Host 'MetaRoot Total Time: ' $($EndMetaRoot - $StartMetaRoot).totalSeconds
+
+
+$StartMetaRecurse = Get-Date
+(Get-ChildItem $FilePath -Recurse -Directory) | ForEach-Object {
+    Write-Progress -Activity 'Staring Jobs for subFolders' -CurrentOperation $_.Name
+    [void](WF_MetadataReportFile -FolderPath $_.FullName -AsJob)
+}
+Write-Host 'Please Wait for All Jobs to Complete' -back darkcyan -for white
+$MetaRecurse = Get-Job | Wait-Job | Receive-Job
+$MetaRecurse.Count
+if (($MetaRecurse | Measure-Object).count -gt 0) {
+    $MetaRecurse.psobject.Properties.Remove('PSComputerName')
+    $MetaRecurse.psobject.Properties.Remove('PSShowComputerName')
+    $MetaRecurse.psobject.Properties.Remove('PSSourceJobInstanceId')
+}
+Get-Job | Remove-Job
+$EndMetaRecurse = Get-Date
+Write-Host 'MetaRecurse Total Time: ' $($EndMetaRecurse - $StartMetaRecurse).totalSeconds
+
+#endregion Processing Data
+
+
+#region Export to Excel
+[PSCustomObject]@{
+    ReportCreated   = $(Get-Date -f 'dddd dd MMMM yyyy hh:mm:ss tt')
+    VolumeName      = $VolumeName
+    RootFolder      = $FilePath
+    TotalFilesCount = ($MetaRoot | Measure-Object).count + ($MetaRecurse | Measure-Object).count
+} | Export-Excel -Path $Dir -WorksheetName 'MEDIA STAT' -Title 'Media Statistics' -AutoSize -TableStyle Dark1
+
+[PSCustomObject]@{
+    StartTime   = $(Get-Date $StartMetaRoot -f 'dddd dd MMMM yyyy hh:mm:ss tt')
+    EndTime      = $(Get-Date $EndMetaRecurse -f 'dddd dd MMMM yyyy hh:mm:ss tt')
+    RootFolder      = $FilePath
+    TotalTime  = $($EndMetaRecurse-$StartMetaRoot)
+} | Export-Excel -Path $Dir -WorksheetName 'TIME STAT' -Title 'Time Statistics' -AutoSize -TableStyle Dark1 -Append
+
+
+$MetaRoot | Where-Object { $_.'Item type' -ne 'ITC2 File' -and $_.'Item type' -ne 'iTunes Database File' } | Select-Object @{N = 'VolumeName';E = { $VolumeName } },* | Sort-Object Folder,Name | Export-Excel -Path $Dir -WorksheetName 'MEDIA METADATA' -AutoSize -TableStyle Light10
+$MetaRecurse | Where-Object { $_.'Item type' -ne 'ITC2 File' -and $_.'Item type' -ne 'iTunes Database File' } | Select-Object @{N = 'VolumeName';E = { $VolumeName } },* | Sort-Object Folder,Name | Export-Excel -Path $Dir -WorksheetName 'MEDIA METADATA' -AutoSize -TableStyle Light10 -Append
+
+#endregion Export to Excel
+
+
+# Fun Staff
+1..3 | ForEach-Object { [console]::beep(600, 1000) ; Start-Sleep -Milliseconds 50 }
+
+Stop-Transcript
