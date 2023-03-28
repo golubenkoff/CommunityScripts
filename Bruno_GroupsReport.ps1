d3f33a177c00b741507433acc837505b21b919e0
Andriy Golubenkov FUIB VM <ag@pumb.ua>
2023-03-28 12:41:44 +0300

    GroupsReport

---
+++ Bruno_GroupsReport.ps1
@@ -0,0 +1,29 @@
+﻿param(
+    [parameter(Mandatory = $true,HelpMessage = 'Organizational Unit Name')]$OU
+)
+
+#requires -module ActiveDirectory
+
+$ScriptPath = Split-Path $MyInvocation.MyCommand.Definition
+
+[System.Collections.ArrayList]$Report = @()
+
+if (!([string]::IsNullOrEmpty($OU))) {
+    $OU_ID = $null ; $OU_ID = Get-ADOrganizationalUnit -Filter { Name -eq $OU }
+    if ($OU_ID) {
+            (Get-ADGroup -SearchBase $OU_ID -SearchScope Subtree -Filter *) | ForEach-Object {
+            $GroupName = $null ; $GroupName = $_
+            $GroupName | Get-ADGroupMember | ForEach-Object {
+                [void]$Report.Add([PSCustomObject]@{
+                        GroupName            = $GroupName.Name
+                        MemberSamAccountName = $_.SamAccountName
+                        MemberObjectClass    = $_.ObjectClass
+                    })
+            }
+        }
+    }
+}
+
+if ($Report) {
+    $Report | Export-Csv -Path $(Join-Path $ScriptPath "Report_OU_${OU}_$(Get-Date -f 'ddMMyyyyHHmm').csv") -UseCulture -Encoding UTF8 -NoTypeInformation
+}
---
+++ FilesMetaReport/WF_Generate-MetadataReport.Tests.ps1
@@ -0,0 +1,12 @@
+﻿Describe "Required PowerShell module" {
+    Context "Check 'ImportExcel' is installed" {
+        It "is Module Found" {
+            # Act
+            $module = Get-Module -Name ImportExcel -ListAvailable
+
+            # Assert
+            $module | Should -Not -BeNullOrEmpty
+
+        }
+    }
+}
