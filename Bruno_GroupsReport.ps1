7465e0912e27c6aeb8e17c4c54bfee910880823a
Andriy Golubenkov FUIB VM <ag@pumb.ua>
2023-03-28 15:07:05 +0300

    Upd GroupsReport

--- Bruno_GroupsReport.ps1
+++ Bruno_GroupsReport.ps1
@@ -9,9 +9,13 @@ $ScriptPath = Split-Path $MyInvocation.MyCommand.Definition
 [System.Collections.ArrayList]$Report = @()
 
 if (!([string]::IsNullOrEmpty($OU))) {
-    $OU_ID = $null ; $OU_ID = Get-ADOrganizationalUnit -Filter { Name -eq $OU }
+    $OU_ID = $null ; $OU_ID = if ($OU -match '^OU=.*') { Get-ADOrganizationalUnit $OU }else { Get-ADOrganizationalUnit -Filter { Name -eq $OU } -ResultSetSize $null }
+    if (($OU_ID | Measure-Object).count -gt 1) {
+        $OU_ID = $OU_ID | Out-GridView -Title 'Select OU' -OutputMode Single
+    }
+
     if ($OU_ID) {
-            (Get-ADGroup -SearchBase $OU_ID -SearchScope Subtree -Filter *) | ForEach-Object {
+            (Get-ADGroup -SearchBase $OU_ID -SearchScope Subtree -Filter * -ResultSetSize $null ) | ForEach-Object {
             $GroupName = $null ; $GroupName = $_
             $GroupName | Get-ADGroupMember | ForEach-Object {
                 [void]$Report.Add([PSCustomObject]@{
@@ -21,7 +25,7 @@ if (!([string]::IsNullOrEmpty($OU))) {
                     })
             }
         }
-    }else{
+    } else {
         Write-Host "OU Not Found: $OU" -BackgroundColor Red -ForegroundColor White
     }
 }
